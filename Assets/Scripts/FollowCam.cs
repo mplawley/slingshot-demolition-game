af3e7609e5372826c407c2e94930cdf85735c508
mplawley <mark.lawley@gmail.com>
2016-09-20 18:29:32 -0400

    Slingshot and camera functionality built

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour
+{
+	#region Fields
+	static public FollowCam S; //singleton
+
+	[Header("Inspector fields")]
+	public float easing = 0.05f;
+	public Vector2 minXY;
+
+	[Header("Dynamic fields")]
+	public GameObject poi; //The point of interest
+	public float camZ; //The desired Z pos of the camera
+	#endregion
+
+	#region Methods
+
+	//Create the singleton
+	void Awake()
+	{
+		S = this;
+		camZ = this.transform.position.z;
+	}
+		
+	void Start()
+	{
+	
+	}
+
+	void FixedUpdate()
+	{
+		if (poi == null)
+		{
+			return;
+		}
+
+		//Get the position of the poi
+		Vector3 destination = poi.transform.position;
+
+		//Limit the X and Y to minimum values
+		destination.x = Mathf.Max(minXY.x, destination.x);
+		destination.y = Mathf.Max(minXY.y, destination.y);
+
+		//Interpolate from the current Camera position toward destination
+		destination = Vector3.Lerp(transform.position, destination, easing);
+
+		//Retain a destination.z of camZ
+		destination.z = camZ;
+
+		//Set the camera to the destination
+		transform.position = destination;
+
+		//Set the orthographic size of the camera
+		//TODO
+	}
+	#endregion
+}
